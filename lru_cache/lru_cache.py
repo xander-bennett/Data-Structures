1e74642f1897c095a5a14a4728faba589b84a190
xander-bennett <xander-bennett@users.noreply.github.com>
2020-03-24 16:32:56 -0700

    Update lru_cache.py

--- lru_cache/lru_cache.py
+++ lru_cache/lru_cache.py
@@ -1,3 +1,5 @@
+from doubly_linked_list import DoublyLinkedList
+
 class LRUCache:
     """
     Our LRUCache class keeps track of the max number of nodes it
@@ -7,7 +9,10 @@ class LRUCache:
     to every node stored in the cache.
     """
     def __init__(self, limit=10):
-        pass
+        self.limit = limit
+        self.length = 0
+        self.cache = {}
+        self.storage = DoublyLinkedList()
 
     """
     Retrieves the value associated with the given key. Also
@@ -17,7 +22,12 @@ class LRUCache:
     key-value pair doesn't exist in the cache.
     """
     def get(self, key):
-        pass
+        if key not in self.cache:
+            return None
+        else:
+            node = self.cache[key]
+            self.storage.move_to_end(node)
+            return node.value[1]
 
     """
     Adds the given key-value pair to the cache. The newly-
@@ -30,4 +40,15 @@ class LRUCache:
     the newly-specified value.
     """
     def set(self, key, value):
-        pass
+        if key in self.cache:
+            node = self.cache[key]
+            self.storage.move_to_end(node)
+            node.value = [key, value]
+
+        if self.limit == self.length:
+            del self.cache[self.storage.head.value[0]]
+            self.storage.remove_from_head()
+
+        self.length += 1
+        self.storage.add_to_tail((key,value))
+        self.cache[key] = self.storage.tail
